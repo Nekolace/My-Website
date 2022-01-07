a981316ede60450b45dde5c76fe031833c593e45
Nekolace <nickyd528@aol.com>
2022-01-07 06:03:02 -0500

    Added Footer and EVEN MORE mobile compatibility.

---
+++ src/Sections/Footer.js
@@ -0,0 +1,21 @@
+import '../MainCSS/Footer.css';
+import React from 'react';
+
+
+export default class Footer extends React.Component{
+
+    render(){
+        return(
+            <div className='footer-container'>
+                <div className='footer'>
+                    <h1>Thanks for checking me out. I hope you enjoyed the puns and shoddy humor. There's more of that if you work with me, I promise.</h1>
+
+                    <h1>nick dimegl.io signing out</h1>
+
+                    <h2>&#10084;&#65039;</h2>
+                </div>
+            </div>
+        )
+    }
+
+}
--- src/Viewables/Home.js
+++ src/Viewables/Home.js
@@ -2,15 +2,17 @@ import '../MainCSS/Home.css';
 import React from 'react';
 import Intro from '../Sections/Intro'
 import HumbleBragSection from '../Sections/HumbleBragSection';
+import Footer from '../Sections/Footer';
 
 export default class Home extends React.Component{
 
 
     render(){
         return(
-          <div>
+          <div className='home-container'>
             <Intro/>
             <HumbleBragSection/>
+            <Footer/>
           </div>
         )
     }
